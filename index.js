2c57808d241f7d3fa38d4ca8cef1b29f71e256c2
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-15 00:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your solution here!
 const drivers =  ["Milo", "Otis", "Garfield"]
-let destructivelyAppendDriver("Drivers") = drivers.push("Drivers")
+let destructivelyAppendDriver() = drivers.push("Drivers")
